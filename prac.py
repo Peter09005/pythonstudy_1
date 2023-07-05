b814bbe2d8285844ba0b89596586aa54b19e7d65
peter00905 <hkt010905@gmail.com>
2023-07-06 02:32:57 +0900

    [황규태] 파이썬 파일 업로드

---
+++ prac.py
@@ -0,0 +1,14 @@
+print(123);
+
+print('Hello World');
+
+x = 12.2
+y = 14
+x = 100
+
+# Goods : spam eggs spam23 _speed
+# SPAM != spaM
+
+# 가독성이 중요함
+# choosing variable to help reader to understand much better
+
