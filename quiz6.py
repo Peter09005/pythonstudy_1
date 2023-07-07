d0094cf0d3e611355279eaaf5525d29be59383ca
peter00905 <hkt010905@gmail.com>
2023-07-07 18:59:59 +0900

    [황규태] 지역전역 , quiz 추가합니다

---
+++ quiz6.py
@@ -0,0 +1,11 @@
+def std_weight(height,gender):
+    height_meter = height/100
+    if gender == "남자":
+        return (height_meter**2)*22
+    else:
+        return (height_meter**2)*21
+
+gender = input("성별 입력 ")
+height = int(input("키 입력 "))
+
+print("키 {0}cm {1}의 표준 체중은 {2}kg 입니다".format(height,gender,std_weight(height,gender)))
